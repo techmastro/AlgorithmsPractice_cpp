b501028ee64a2c21f7a983284ea53b9f712c6baf
learningmobility <learninggrowth@gmail.com>
2023-10-02 18:25:44 +0530

    Added and tested insertion sort.

--- AlgorithmsPractice/Main.cpp
+++ AlgorithmsPractice/Main.cpp
@@ -8,9 +8,13 @@ using namespace std;
 int main() {
 	cout << "Welcome to Algorithm Practice\n========================\n\n\n";
 
+	cout << "===== Insertion Sort Example >>>>>\n\n\n";
+	insertionSort_Main();
+	cout << "\n\n\n<<<<< Insertion Sort Example =====\n\n\n";
+
 	cout << "===== Bubble Sort Example >>>>>\n\n\n";
 	bubbleSort_Main();
-	cout << "\n\n\n<<<<< Linked List Example =====\n\n\n";
+	cout << "\n\n\n<<<<< Bubble Sort Example =====\n\n\n";
 
 	cout << "===== Linked List Example >>>>>\n\n\n";
 	linkedList_Main();
--- AlgorithmsPractice/Sort.cpp
+++ AlgorithmsPractice/Sort.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
 using namespace std;
 
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/**
+ * Insertion sort.
+ * input:
+ * arr - pointer to array.
+ * size - size of the array.
+*/
+void insertionSort(int *a, int size){
+    if(size<2)
+        return;
+    for(int i=1;i<size;i++){
+        int j = i-1;
+        int temp = *(a+i);
+        while(j>=0 && temp < *(a+j)){
+            *(a+j+1) = *(a+j);
+            j--;
+        }
+        *(a+j+1) = temp;
+    }
+}
+
+void insertionSort_Main(){
+    int a[] = {5,3,7,9,11,6,1,13,21,2};
+
+    cout<<"Insertion Sort\n";
+    cout<<"--------------\n\n";
+    cout<<"Before Sorting:\n";
+    for(auto i: a)
+        cout<<i<<", ";
+
+    insertionSort(a,10);
+
+    cout<<"\n\nAfter Sorting:\n";
+    for(auto i: a)
+        cout<<i<<", ";
+}
 
 /**
  * Bubble sorting algorithm.
@@ -23,6 +64,8 @@ void bubbleSort(int *arr, int size){
 void bubbleSort_Main(){
     int a[] = {5,3,7,9,11,6,1,13,21,2};
 
+    cout<<"Bubble Sort\n";
+    cout<<"--------------\n\n";
     cout<<"Before Sorting:\n";
     for(auto i: a)
         cout<<i<<", ";
